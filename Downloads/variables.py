b59d0cba8784d587dbceb67f94f751a0a4b4652a
Aman Bandali <ab8068@rit.edu>
2025-09-09 09:48:11 +0400

    variables

---
+++ Downloads/variables.py
@@ -0,0 +1,12 @@
+def variable_practice():
+    Age = int(18)
+    number_days_year = float(365.25)
+    Name = str("Aman")
+    pi= float(3.1415)
+    print("Age is:" , Age)
+    print("Number of days in a year is:" , number_days_year)
+    print("My name is:" , Name)
+    print("Value of pi is:" , pi)
+
+
+variable_practice()
