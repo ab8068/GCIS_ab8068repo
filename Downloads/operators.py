78e6dc601a5c8aa8ed9d602f311ce8c42a9eaeb0
Aman Bandali <ab8068@rit.edu>
2025-09-09 10:02:11 +0400

    operators name change

--- Downloads/operators.py
+++ Downloads/operators.py
@@ -1,12 +1,12 @@
 def operators():
     a = 10
     b = 20
-    print("a + b =", a + b)
-    print("a - b =", a - b)
-    print("a * b =", a * b)
-    print("a / b =", a / b)
-    print("a // b =", a // b)
-    print("a % b =", a % b)
-    print("a ** b =", a ** b)
+    print("Addition:", a + b)
+    print("Subtraction:", a - b)
+    print("Multiplication:", a * b)
+    print("Division:", a / b)
+    print("Floor Division:", a // b)
+    print("Modulus:", a % b)
+    print("Exponent:", a ** b)
 
 operators()
